8881106cd2b9a6c079bea6525552da535bf4af19
RareGemAF <kitiibwa2012@gmail.com>
2025-09-10 15:55:44 +0300

    more on json response

--- index.js
+++ index.js
@@ -4,6 +4,43 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+
+app.get('/about', (req, res) =>{
+  res.send('This is my about page');
 });
+
+app.get('/contact', (req, res) =>{
+  res.send('This is my contact page');
+});
+
+//Route parameters
+app.get('/user/:id', (req, res) =>{
+  const query = req.query.id;
+  res.send('You searched for: ' + userId);
+})
+
+//Query parameters
+app.get('/search', (req, res) =>{
+  const query = req.query.q;
+  res.send('You searched for: ' + query);
+})
+
+
+// JSON response
+app.get('/products', (req, res) => {
+  const products =[
+    {id: 1, name: 'product 1', price: 100},
+    {id: 1, name: 'product 2', price: 100},
+    {id: 1, name: 'product 3', price: 100},
+  ];
+  res.json(products)
+})
+
+
+
+
+
+app.listen(port, () => {
+  console.log("Server is running on port" + port);
+})
+
